60134a0d129a1afe53288e632773e08e5fe026ed
BigglesDevs <103225055+BigglesDevs@users.noreply.github.com>
2024-06-04 14:08:01 +0100

    added fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BigglesDevelopment 💖'
+description 'A tint meter script for checking car tints.'
+version '1.0.0 (Free)'
+
+client_script 'client/client.lua'
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/index.js',
+    'html/style.css'
+}
